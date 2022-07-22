20595913d3f7f309973a1442c75a917bf7cc30b3
Osvaldo Zakowicz <osvaldozakowicz@gmail.com>
2022-07-22 17:53:29 -0300

    agregada la interfaz heroe, y el componente heroes, editada la plantilla con una visualizacion y edicion basica del heroe

---
+++ src/app/Hero.ts
@@ -0,0 +1,9 @@
+export interface Hero {
+  id: number,
+  name: string
+};
+
+/**
+ * Esta interfaz representa un Heroe
+ * con un id y un nombre.
+ */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//para usar el enlace bidireccional ngModel, poner tambien en el array imports: []
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
