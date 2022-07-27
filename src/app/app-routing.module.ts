a587068d0f73e0e35645727f45dc8c6f6d2e51b2
Osvaldo Zakowicz <osvaldozakowicz@gmail.com>
2022-07-27 14:22:27 -0300

    agregado un modulo de ruteo app routing e importado a app module, configurado con la primera ruta a app component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+//*no es necesario CommonModule, tambien se quitan las declaraciones
+//import { CommonModule } from '@angular/common';
+//funcionalidad de enrutamiento
+import { RouterModule, Routes } from '@angular/router';
+//a donde ir luego de configurar las rutas
+import { HeroesComponent } from './heroes/heroes.component';
+
+/**
+ * *A continuacion se configuran las rutas.
+ * constante de tipo Routes, es un array de objetos Route con propiedades path y component.
+ * *las rutas indican al enrutador que vista mostrar, tienen dos partes: 
+ * - path: cadena que coincide con una URL en la barra de direcciones del navegador.
+ * - component: el componente que el enrutador debe crear al navegar en esa ruta.
+ */
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent},
+];
+
+/**
+ * *En Angular, la mejor práctica es cargar y configurar el enrutador en un módulo de 
+ * *nivel superior separado que está dedicado al enrutamiento e importado por la raíz AppModule.
+ * Por convención, el nombre de la clase del módulo es AppRoutingModule y 
+ * pertenece a app-routing.module.ts en la carpeta src/app.
+ * ?como se genera este archivo:
+ * * ng generate module app-routing --flat --module=app
+ * * --flat indica que el archivo debe estar es src/app y no en su propia carpeta
+ * * --module=app indica que se importe este modulo de rutas en AppComponent
+ */
+
+/**
+ * *Los metadatos de NgModule inicializan el enrutador y lo hacen escuchar los cambios
+ * de ubicacion del navegador.
+ * importa: RouterModule y lo configura para el arreglo de rutas anterior.
+ * luego exporta RouterModule configurado.
+ * 
+ * *NOTA: El metodo se llama forRoot() porque configura el enrutador en el nivel raiz 
+ * *de la aplicacion. El metodo forRoot() proporciona los proveedores de servicios y las 
+ * *directivas necesarias para el enrutamiento, y realiza la navegacion inicial basada en 
+ * *la URL del navegador actual.
+ */
+@NgModule({
+  //declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
